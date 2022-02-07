b0d48e4847601fb0a349c604cb6197fde7722cb4
kazuki-77 <k.sora.7861@gmail.com>
2022-02-07 01:58:23 +0000

    [add]ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+
+  get 'chat/:id' => 'chats#show', as: '/chat'
+  resources :chats, only: [:create]
+
+  resources :users, only: [:show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get :followeds, on: :member
+    get :followers, on: :member
+  end
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    # showページが不要、idの受け渡しもないのでresourceと記述
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
 end
