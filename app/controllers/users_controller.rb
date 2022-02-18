885a479e1cc33b2e947c69f39c9711530de5a5f4
kazuki-77 <k.sora.7861@gmail.com>
2022-02-18 14:49:29 +0000

    [add] レスポンシブ記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images.page(params[:page]).reverse_order
+    @post_images = @user.post_images.page(params[:page]).reverse_order.per(4)
   end
 
   def edit
