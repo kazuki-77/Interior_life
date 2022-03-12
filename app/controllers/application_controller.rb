e8d3c9cdbebf08968e0025bc5d8345b09ff3dfb6
kazuki-77 <k.sora.7861@gmail.com>
2022-03-12 07:55:59 +0000

    [fix]ログイン後遷移先ページ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
     # sign_upの際に、nameのデータ操作が許可される
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 end
